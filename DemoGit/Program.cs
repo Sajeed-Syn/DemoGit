68f17e349702dcc59be1d5d1ab25a2d5f842272f
Sajeed <sajeedrs@gmail>
2021-11-14 20:38:43 +0530

    Saved 8 oclock

--- DemoGit/Program.cs
+++ DemoGit/Program.cs
@@ -6,7 +6,8 @@ namespace DemoGit
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to Git!");
+            Console.WriteLine("Hello Git");
         }
     }
 }
